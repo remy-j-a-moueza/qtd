32288bab56739bbea9cf3412f8e320744001afe4
mandel <devnull@localhost>
2009-06-12 22:21:33 +0000

    ldc -> dmd

--- examples/itemviews/customsortfiltermodel/build.sh
+++ examples/itemviews/customsortfiltermodel/build.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-ldc main.d mysortfilterproxymodel.d window.d -I../../../ -I../../../qt/d1 -L-L../../../lib -L-lqtdgui -L-lqtdcore -L-lQtCore -L-lQtGui -ofmysortfilterproxymodel
+dmd main.d mysortfilterproxymodel.d window.d -I../../../ -I../../../qt/d1 -L-L../../../lib -L-lqtdgui -L-lqtdcore -L-lQtCore -L-lQtGui -ofmysortfilterproxymodel
