7a8ef7466012d1c45e0aac2096a4777b721b554b
eldar <devnull@localhost>
2009-07-14 22:00:46 +0000

    no static ctors allowed :F

--- qt/gui/UrlHandler.d
+++ qt/gui/UrlHandler.d
@@ -6,6 +6,9 @@ alias void delegate(QUrl) UrlHandlerDg;
 
 package class UrlHandler : QObject {
     public this(UrlHandlerDg dg) {
+        if (!init_flag_UrlHandler)
+            static_init_UrlHandler();
+
         _dg = dg;
         void* __qt_return_value = qtd_UrlHandler_UrlHandler_QObject(cast(void*) this, null);
         this(__qt_return_value, true);
@@ -44,11 +47,14 @@ private extern(C) void qtd_UrlHandler_handleUrl_QUrl_dispatch(void *d_entity, vo
 
 private extern (C) void qtd_UrlHandler_initCallBacks(void* virtuals, void* qobj_del);
 
-static this() {
+private bool init_flag_UrlHandler = false;
+void static_init_UrlHandler() {
+    init_flag_UrlHandler = true;
+
     void*[1] virt_arr;
     virt_arr[0] = &qtd_UrlHandler_handleUrl_QUrl_dispatch;
 
-    void *qobj_del;
+//    void *qobj_del;
 //    qobj_del = &qtd_D_QWidget_delete;
     qtd_UrlHandler_initCallBacks(virt_arr.ptr, null);
 }
